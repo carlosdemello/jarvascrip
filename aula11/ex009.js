4d73a5ef49fa3f157e52dccf7bd49a6a64af7b6b
carlosdemello <108680766+carlosdemello@users.noreply.github.com>
2022-07-20 17:41:25 -0300

    kkk

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+console.log(`   `);
+if (país != 'Brasil') {
+    console.log(`Voce é estrangeiro!`);
+}
+else {
+    console.log(`Voce é Brasileiro!`);
+}
---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 10 
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+if (vel > 60) {
+    console.log(`Voce Uttrapassou a velocidade permitida. Multado!`);
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
